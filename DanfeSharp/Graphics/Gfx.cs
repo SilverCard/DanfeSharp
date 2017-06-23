cea6c9faf85338c24d7a85db329a81928de3a703
Ricardo Brito <ricardo.b@msn.com>
2017-06-23 20:38:42 -0300

    Melhorias em Gfx.

--- DanfeSharp/Graphics/Gfx.cs
+++ DanfeSharp/Graphics/Gfx.cs
@@ -1,7 +1,7 @@
-﻿using org.pdfclown.documents.contents.composition;
-using System.Drawing;
+﻿using System.Drawing;
 using System;
 using org.pdfclown.documents.contents.xObjects;
+using org.pdfclown.documents.contents.composition;
 
 namespace DanfeSharp.Graphics
 {
@@ -9,19 +9,16 @@ namespace DanfeSharp.Graphics
     {
         public PrimitiveComposer PrimitiveComposer { get; private set; }
 
-        public Gfx(PrimitiveComposer xg)
+        public Gfx(PrimitiveComposer primitiveComposer)
         {
-            PrimitiveComposer = xg;          
-        }
-
-        public void DrawRectangle(float x, float y, float w, float h) => DrawRectangle(new RectangleF(x, y, w, h));
-        public void DrawRectangle(RectangleF rect) => PrimitiveComposer.DrawRectangle(rect.ToPointMeasure());
-        public void Fill() => PrimitiveComposer.Fill();
-            
+            PrimitiveComposer = primitiveComposer ?? throw new ArgumentNullException(nameof(primitiveComposer));          
+        }            
 
         internal void DrawString(string str, RectangleF rect, Fonte fonte, AlinhamentoHorizontal ah = AlinhamentoHorizontal.Esquerda, AlinhamentoVertical av = AlinhamentoVertical.Topo)
         {
+            if (fonte == null) throw new ArgumentNullException(nameof(fonte));
             if (fonte.Tamanho <= 0) throw new ArgumentOutOfRangeException(nameof(fonte));
+            CheckRectangle(rect);
 
             var p = rect.Location;
 
@@ -30,18 +27,34 @@ namespace DanfeSharp.Graphics
             else if (av == AlinhamentoVertical.Centro)
                 p.Y += (rect.Height - fonte.AlturaLinha) / 2F;
 
-
             if (ah == AlinhamentoHorizontal.Direita)
                 p.X = rect.Right - fonte.MedirLarguraTexto(str);
             if (ah == AlinhamentoHorizontal.Centro)
                 p.X += (rect.Width - fonte.MedirLarguraTexto(str)) / 2F;
 
+            SetFont(fonte);
+            ShowText(str, p);
+        }
+
+        public void SetFont(Fonte fonte)
+        {
+            if (fonte == null) throw new ArgumentNullException(nameof(fonte));
+            if (fonte.FonteInterna == null) throw new ArgumentNullException(nameof(fonte));
+            if (fonte.Tamanho <= 0) throw new ArgumentNullException(nameof(fonte));
             PrimitiveComposer.SetFont(fonte.FonteInterna, fonte.Tamanho);
-            PrimitiveComposer.ShowText(str, p.ToPointMeasure());
+        }
+
+        public void ShowText(String text, PointF point)
+        {
+            CheckPoint(point);
+            PrimitiveComposer.ShowText(text, point.ToPointMeasure());
         }
 
         public void ShowXObject(XObject xobj, RectangleF r)
         {
+            if (xobj == null) throw new ArgumentNullException(nameof(xobj));
+            CheckRectangle(r);
+
             PointF p = new PointF();
             SizeF s = new SizeF();
             SizeF xs = xobj.Size.ToMm();
@@ -82,12 +95,37 @@ namespace DanfeSharp.Graphics
         public void StrokeRectangle(RectangleF rect, float width)
         {
             SetLineWidth(width);
-            PrimitiveComposer.DrawRectangle(rect.ToPointMeasure());
+            DrawRectangle(rect);
             Stroke();
         }
 
+        public void SetLineWidth(float w)
+        {
+            if (w < 0) throw new ArgumentOutOfRangeException(nameof(w));
+            PrimitiveComposer.SetLineWidth(w);
+        }
+
+        public void DrawRectangle(RectangleF rect)
+        {
+            CheckRectangle(rect);
+            PrimitiveComposer.DrawRectangle(rect.ToPointMeasure());
+        }
+
+        private void CheckRectangle(RectangleF r)
+        {
+            if (r.X < 0 || r.Y < 0 || r.Width <= 0 || r.Height <= 0) throw new ArgumentException(nameof(r));
+        }
+
+        private void CheckPoint(PointF p)
+        {
+            if (p.X < 0 || p.Y < 0) throw new ArgumentException(nameof(p));
+        }
+
         public void Stroke() => PrimitiveComposer.Stroke();
-        public void SetLineWidth(float w) => PrimitiveComposer.SetLineWidth(w);      
         public void Flush() => PrimitiveComposer.Flush();
+        public void Fill() => PrimitiveComposer.Fill();
+        public void DrawRectangle(float x, float y, float w, float h) => DrawRectangle(new RectangleF(x, y, w, h));
+
+
     }
 }
