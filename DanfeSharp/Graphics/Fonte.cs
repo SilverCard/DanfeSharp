a944a5fbcf196bc7767130d8340957a29c802e07
Ricardo Brito <ricardo.b@msn.com>
2017-06-25 10:35:41 -0300

    Adicionado sumários na classe Fonte.

--- DanfeSharp/Graphics/Fonte.cs
+++ DanfeSharp/Graphics/Fonte.cs
@@ -3,11 +3,27 @@ using System;
 
 namespace DanfeSharp.Graphics
 {
+    /// <summary>
+    /// Define uma fonte do PDF Clown e um tamanho. 
+    /// </summary>
     internal class Fonte
     {
         private float _Tamanho;
 
+        /// <summary>
+        /// Fonte do PDF Clown.
+        /// </summary>
         public pcf.Font FonteInterna { get; private set; }
+
+        public Fonte(pcf.Font font, float tamanho)
+        {
+            FonteInterna = font ?? throw new ArgumentNullException(nameof(font));
+            Tamanho = tamanho;
+        }
+
+        /// <summary>
+        /// Tamanho da fonte.
+        /// </summary>
         public float Tamanho
         {
             get => _Tamanho;
@@ -18,15 +34,27 @@ namespace DanfeSharp.Graphics
             }
         }
 
-        public Fonte(pcf.Font font, float tamanho)
+        /// <summary>
+        /// Mede a largura ocupada por uma string.
+        /// </summary>
+        /// <param name="str">String</param>
+        /// <returns>Largura em mm.</returns>
+        public float MedirLarguraTexto(String str)
         {
-            FonteInterna = font ?? throw new ArgumentNullException(nameof(font));
-            Tamanho = tamanho;
+            if (String.IsNullOrEmpty(str)) return 0;
+            return (float)FonteInterna.GetWidth(str, Tamanho).ToMm();
         }
 
-        public float MedirLarguraTexto(String str) => (float)FonteInterna.GetWidth(str, Tamanho).ToMm();
+        /// <summary>
+        /// Mese a largura ocupada por um Char.
+        /// </summary>
+        /// <param name="c">Char</param>
+        /// <returns>Largura em mm.</returns>
         public float MedirLarguraChar(char c) => (float)FonteInterna.GetWidth(c, Tamanho).ToMm();
-
+        
+        /// <summary>
+        /// Medida da altura da linha.
+        /// </summary>
         public float AlturaLinha => (float)FonteInterna.GetLineHeight(Tamanho).ToMm();
 
 
