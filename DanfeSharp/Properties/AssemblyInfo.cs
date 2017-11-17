1a8e33c4a27b7e63bec918149fe8d9ffc632d4a4
Ricardo Brito <ricardo.b@msn.com>
2017-11-17 09:11:33 -0900

    Aumento de versão para 2.0.1.

--- DanfeSharp/Properties/AssemblyInfo.cs
+++ DanfeSharp/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DanfeSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Gerador de DANFE em PDF.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DanfeSharp")]
@@ -35,7 +35,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
 [assembly: NeutralResourcesLanguage("pt-BR")]
 
